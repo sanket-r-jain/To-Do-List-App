2fd419fb52aa94e720e3c3450aeb0b8fefb8ea88
Sanket Jain <64455761+sanket-r-jain@users.noreply.github.com>
2022-02-22 11:04:25 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
+            <br>
             This is a simple to-do list application.
             <p>Users can add and keep a track of all their pending tasks using this tool. One they have completed a certain task, they can remove it from the list.</p>
             <p>This simple tool has been created by <a href="https://github.com/sanket-r-jain">Sanket Jain</a>. Please feel free to reach out via <a href="https://www.linkedin.com/in/sanket--jain/">LinkedIn</a>, if you would like to share any feedback.</p>
